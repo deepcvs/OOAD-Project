7152a6bb5190a9f711afd52571b1f7a6df6110de
Chinmay <camkoolkarni.com>
2022-04-20 14:57:06 +0530

    menu display

--- Menu.java
+++ Menu.java
@@ -1,29 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
 
-import java.util.*;
-
-/**
- * 
- */
 public class Menu {
-
-    /**
-     * Default constructor
-     */
-    public Menu() {
+    public static void display() throws Exception {
+        Scanner sc = new Scanner(new File("D:\\study\\computer science\\OOAD\\OOAD-Project\\Menu.csv"));
+        sc.useDelimiter(","); // sets the delimiter pattern
+        while (sc.hasNext()) // returns a boolean value
+        {
+            System.out.print(sc.next() + " "); // find and returns the next complete token from this scanner
+        }
+        sc.close(); // closes the scanner
     }
 
-    /**
-     * 
-     */
-    public void details;
-
-    /**
-     * 
-     */
-    public void list_of_dishes;
-
-
-
-
-
+    public static void main(String[] args) throws Exception {
+        display();
+    }
 }
